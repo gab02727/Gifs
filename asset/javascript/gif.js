c63e32916b13bc2a2ba6fd6af8920397005f9739
Grace Oyemosu <gbengagabriel27@gmail.com>
2018-03-07 23:51:56 -0600

    complete assignment

--- asset/javascript/gif.js
+++ asset/javascript/gif.js
@@ -1,11 +1,11 @@
-var animals = ["Tiger", "Monkey", "Lion", "zebra"];
+var topics = ["Travel", "Animals", "Cars", "Buildings"];
 
-function displayAnimalInfo() {
+function displayTopic() {
 
-    var animal = $(this).attr("data-name");
+    var topic = $(this).attr("data-name");
     
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=dc6zaTOxFJmzC&limit=10";
    // console.log(queryURL);
 
 
@@ -22,7 +22,8 @@ function displayAnimalInfo() {
         for(var i = 0; i < response.data.length; i++){
     
 
-        var animalDiv = $("<div>").addClass("animal");
+        var topicDiv = $("<div>").addClass("animal");
+        
 
          // Storing the rating data
          var rating = response.data[i].rating;
@@ -31,20 +32,27 @@ function displayAnimalInfo() {
          var pOne = $("<p>").text("Rating: " + rating);
 
          // Displaying the rating
-         animalDiv.prepend(pOne);
+         topicDiv.prepend(pOne);
 
          // Retrieving the URL for the image
-         var imgURL = response.data[i].images.downsized.url;
+         var animatedURL = response.data[i].images.fixed_height.url;
+         var staticURL = response.data[i].images.fixed_height_still.url;
+         var showImage = $("<img>");
 
          // Creating an element to hold the image
-         var image = $("<img>").attr("src", imgURL);
+         showImage.attr("src", staticURL);
+         showImage.addClass("showGiphy");
+         showImage.attr("data-state", "still");
+         showImage.attr("data-state", staticURL);
+         showImage.attr("data-animate", animatedURL);
+         
       
 
          // Appending the image
-         animalDiv.append(image);
+         topicDiv.append(showImage);
 
-          // Putting the entire animal above the previous movies
-          $("#Animals-view").prepend(animalDiv);
+          // Putting the entire topic above the previous movies
+          $("#Topics-view").prepend(topicDiv);
         }
 
       
@@ -52,51 +60,84 @@ function displayAnimalInfo() {
     });
     }
 
- // Function for displaying animal data
+ // Function for displaying topic data
     function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
+        // Deleting the topics prior to adding new movies
         // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
 
-        // Looping through the array of animals
-        for (var i = 0; i < animals.length; i++) {
+        // Looping through the array of topics
+        for (var i = 0; i < topics.length; i++) {
 
-            // Then dynamicaly generating buttons for each movie in the array
+            // Then dynamicaly generating buttons for each topic in the array
             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var a = $("<button>");
-            // Adding a class of animal-btn to our button
-            a.addClass("animal-btn");
+            // Adding a class of topic-btn to our button
+            a.addClass("topic-btn");
 
              // Adding a data-attribute
-          a.attr("data-name", animals[i]);
+          a.attr("data-name", topics[i]);
           // Providing the initial button text
-          a.text(animals[i]);
+          a.text(topics[i]);
           // Adding the button to the buttons-view div
           $("#buttons-view").append(a);
         }
     }
 
     // This function handles events where an animal button is clicked
-    $("#add-animal").on("click", function(event) {
+    $("#add-topic").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
-        var animal = $("#animal-input").val().trim();
+        var animal = $("#topic-input").val().trim();
 
 
-        // Adding animal from the textbox to our array
-        animals.push(animal);
+        // Adding topics from the textbox to our array
+        topics.push(animal);
         //console.log(animals);
 
-        // Calling renderButtons which handles the processing of our animal array
+        // Calling renderButtons which handles the processing of our topic array
         renderButtons();
-      });
+    });
 
-      // Adding a click event listener to all elements with a class of "animal-btn"
-      $(document).on("click", ".animal-btn", displayAnimalInfo);
+      // Adding a click event listener to all elements with a class of "topic-btn"
+      $(document).on("click", ".topic-btn", displayTopic);
+
+      renderButtons();
+              //Click event on gifs with class of "showGiphy" executes pausePlayGifs function
+              
+        $(document).on("click", ".showGiphy", pausePlayGifs);
+    
+     
+  //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+  function pausePlayGifs() {
+  	 var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+
+        
+
+    }
+};
+
+
+
+
+  
+    
+
 
       // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+      
+
+  
+
+
+    
 
       
 
